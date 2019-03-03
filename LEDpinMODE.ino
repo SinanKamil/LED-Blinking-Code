531086888adfd5838e651672712355c0f4fb8220
Sinan Kamil <44329818+SinanKamil@users.noreply.github.com>
2019-03-03 00:56:14 -0800

    Random Generator
    added a random generator, so the LED is blinks randomly every time.

--- LEDpinMODE.ino
+++ LEDpinMODE.ino
@@ -1,18 +1,24 @@
 
+long ran;
+long ran1;
 int blueLED = 9;
-int onTime = 200;
-int offTime = 300; 
+int onTime = 1000;
+int offTime = 900; 
 
 void setup() {
-  // put your setup code here, to run once:
+// put your setup code here, to run once:
+Serial.begin(800);
   pinMode (blueLED, OUTPUT);
+  randomSeed(analogRead(blueLED));
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    ran = random(onTime);
     digitalWrite(blueLED, HIGH); 
-    delay (onTime);
+    delay (ran);
     digitalWrite(blueLED, LOW);
-    delay (offTime);
+    ran1 = random(offTime);
+    delay (ran1);
 }
